ae309c0a647a3047ffd57383fa5d8f34b138bfdf
Mohammed Shah Jamal <mjamal@office1.visual-meta.com>
2020-04-07 20:57:21 +0200

    First Comit

---
+++ function.js
@@ -0,0 +1,10 @@
+function greet(name, lastName)
+{
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('Mr.', 'Jamal');
+greet('Mrs.', 'Zoya');
+greet('Miss.', 'Mayla');
+
+
+
---
+++ index.js
@@ -0,0 +1,31 @@
+let person = {
+    name: 'Jamal',
+    num: 2
+};
+
+person.name = 'George';
+
+person['name']= 'Marry';
+
+console.log(person.name);
+//console.log(person.age);
+
+/*alert('Jamal study hard and keep Rocking :)');
+document.write('<br>'); 
+document.write(name);
+document.write('<br>'); 
+document.write(num); 
+num = 3;
+document.write('<br>'); 
+document.write(num);
+
+var x = y;
+x = 10;
+document.write(x);
+document.write('<br>')
+document.write(typeof x);*/
+
+
+
+
+
