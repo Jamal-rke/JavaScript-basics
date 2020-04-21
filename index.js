d1945a72a28486a95b834852524d614f0150de0b
Mohammed Shah Jamal <jamal.rke12@gmail.com>
2020-04-21 20:26:20 +0200

    CSS update3

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-let person = {
-    name: 'Jamal',
-    num: 2
-};
-
-person.name = 'George';
-person['name']= 'Marry';
-
-console.log(person.name);
-//console.log(person.age);
-
-/*alert('Jamal study hard and keep Rocking :)');
-document.write('<br>'); 
-document.write(name);
-document.write('<br>'); 
-document.write(num); 
-num = 3;
-document.write('<br>'); 
-document.write(num);
-
-var x = y;
-x = 10;
-document.write(x);
-document.write('<br>')
-document.write(typeof x);*/
-
+let person = 'JAMAL';
+console.log(name);
+ 
 
 
 
