b3b4db6ed10703585fa27116b8d419a4db27d6f2
Mohammed Shah Jamal <mjamal@office1.visual-meta.com>
2020-07-26 19:58:11 +0200

    updates

---
+++ JS/PrimitiveDatatype.js
@@ -0,0 +1,9 @@
+/* strings , numbers
+
+*/
+
+let name = 'Mohmmed'; //String Literal
+let age = 30; // Number Literal
+let isApproved = true; //boolean . it can be true or false
+let firstName = undefined;
+let selectColour = null; //it is used in situation where we have to explicitly clear the value of a variable
